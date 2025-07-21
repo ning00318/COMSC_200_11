1fb6a2c6d990826d0028c4a562affac628b10611
NINA <ninatsao@ningningning.local>
2025-07-21 15:35:39 -0700

    3rd commit

--- CheckingAccount.cpp
+++ CheckingAccount.cpp
@@ -2,16 +2,11 @@
 #include "CheckingAccount.h"
 using namespace std;
 
-// Comment 7: Checking account inherits from the base class, Account, and additionally,
-//            checking account includes a fee charged for each transaction.
 CheckingAccount::CheckingAccount(double initialBalance, double fee)
     : Account(initialBalance), transactionFee(fee)
 {
 }
 
-// Comment 8: When adding an amount, we invoke credit() from the class Account, and
-//            charge the transaction fee by calling debit(). After finishing adding
-//            value, print out a message to display how much the fee was charged.
 void CheckingAccount::credit(double addMoney)
 {
     Account::credit(addMoney);
@@ -19,15 +14,49 @@ void CheckingAccount::credit(double addMoney)
     cout << "$" << transactionFee << " transaction fee charged." << endl;
 }
 
-// Comment 9: When withdraw an amount, we invoke debit() from the class Account to
-//            check if the request value is reasonable.
 void CheckingAccount::debit(double withdrawMoney)
 {
-    // Comment 10: If it returns true, it means the money was withdrawn successfully, and then it
-    //             will charge the transaction fee and print out a message. Otherwise, nothing happens.
     if (Account::debit(withdrawMoney))
     {
         Account::debit(transactionFee);
         cout << "$" << transactionFee << " transaction fee charged." << endl;
     }
+}
+
+CheckingAccount &CheckingAccount::operator+=(const double& addMoney)
+{
+    credit(addMoney);
+    return *this;
+}
+
+CheckingAccount &CheckingAccount::operator-=(const double& withdrawMoney)
+{
+    debit(withdrawMoney);
+    return *this;
+}
+
+CheckingAccount CheckingAccount::operator+(const double& addMoney) const
+{
+    CheckingAccount result(getBalance(), transactionFee);
+    result.credit(addMoney);
+    return result;
+}
+
+CheckingAccount CheckingAccount::operator-(const double& withdrawMoney) const
+{
+    CheckingAccount result(getBalance(), transactionFee);
+    result.debit(withdrawMoney);
+    return result;
+}
+
+CheckingAccount &CheckingAccount::operator=(const CheckingAccount& account2)
+{
+    Account::operator=(account2);
+    return *this;
+}
+
+std::ostream &operator<<(std::ostream &output, const CheckingAccount &account)
+{
+    output << account.getBalance();
+    return output;
 }
--- CheckingAccount.h
+++ CheckingAccount.h
@@ -5,10 +5,17 @@
 // Derived class
 class CheckingAccount : public Account
 {
+    friend std::ostream &operator<<(std::ostream &, const CheckingAccount &);
 public:
     CheckingAccount(double initialBalance, double fee);
     void credit(double addMoney);
     void debit(double withdrawMoney);
+
+    CheckingAccount &operator+=(const double& addMoney);
+    CheckingAccount &operator-=(const double& withdrawMoney);
+    CheckingAccount operator+(const double& addMoney) const;
+    CheckingAccount operator-(const double& withdrawMoney) const;
+    CheckingAccount &operator=(const CheckingAccount&);
 private:
     double transactionFee;
 };
